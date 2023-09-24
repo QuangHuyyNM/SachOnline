a6968c67f0e4450d20909a8a979ac00abd739ac0
QuangHuyyNM <huynm2307@gmail.com>
2023-09-24 23:23:55 +0700

    init project

---
+++ Lap01/Controllers/SachOnlineController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lap01.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SachBanNhieuPartial()
+        {
+            // Xử lý logic và trả về PartialView tương ứng
+            return PartialView("SachBanNhieuPartial");
+        }
+
+    }
+}
